d532ac3d3b00b6fe5cc9b01ca9acccfe0bf25fa1
Dima <dserp18@gmail.com>
2018-10-23 14:24:51 +0300

    Update and rename str_arr.c to arr.c

--- arr.c
+++ arr.c
@@ -3,10 +3,19 @@ int main(void) {
   int i=0;
   char str_arr[100] = "ABCDF";
 
+  int data_arr[10]={0}; 
+  data_arr[0]=2;
+  data_arr[1]=5;
+
 
 while ( str_arr[i] != '\0') { 
     printf( "%c", str_arr[i] );
     i++;          
   }
+  printf("\n");
+  for (int k = 0; k<10; k++)
+    printf("%d ", data_arr[k]);
+  
+  
   return 0;
 }
